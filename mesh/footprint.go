b3bf27abfc43047b83c62939960184243d59ff3a
Georges Winkenbach <georges.winkenbach@gmail.com>
2022-03-14 14:07:00 -0600

    Add convenience accessors to class Footprint

--- mesh/footprint.go
+++ mesh/footprint.go
@@ -19,3 +19,28 @@ func NewFootprint(p1, p2 geom.Pt2) Footprint {
 		PMax: geom.NewPt2(math.Max(p1.X, p2.X), math.Max(p1.Y, p2.Y)),
 	}
 }
+
+// Return the footprint's center point.
+func (f *Footprint) GetCenterPoint() geom.Pt2 {
+	return geom.NewPt2((f.PMin.X+f.PMax.X)*0.5, (f.PMin.Y+f.PMax.Y)*0.5)
+}
+
+// Return the footprint's x-range as (xMin, xMax).
+func (f *Footprint) GetXRange() (float64, float64) {
+	return f.PMin.X, f.PMax.X
+}
+
+// Return the footprint's y-range as (yMin, yMax).
+func (f *Footprint) GetYRange() (float64, float64) {
+	return f.PMin.Y, f.PMax.Y
+}
+
+// Return the foorptint width xMax - xMin
+func (f *Footprint) GetWidth() float64 {
+	return f.PMax.X - f.PMin.X
+}
+
+// Return the foorptint height yMax - yMin
+func (f *Footprint) GetHeight() float64 {
+	return f.PMax.Y - f.PMin.Y
+}
